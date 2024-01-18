c037809e52e2de71c3437b18a249c76920e14798
ElevenNotes <github@11notes.ch>
2024-01-18 08:47:37 +0100

    add readme

--- rootfs/usr/local/bin/healthcheck.sh
+++ rootfs/usr/local/bin/healthcheck.sh
@@ -1,2 +1,11 @@
 #!/bin/ash
-  curl --max-time 5 -kILs --fail http://localhost:8080/ping
+  cURL=$(curl --max-time 5 --silent --output /dev/stderr --write-out "%{http_code}" http://localhost:8080/)
+  case "${cURL}" in
+    200|401)
+      exit 0
+    ;;
+
+    *)
+      exit 1
+    ;;
+  esac
